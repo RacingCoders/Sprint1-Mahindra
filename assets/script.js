aa66f06840a33aa7e32b9d37692be684ad7a587f
Kheyla Thais <kheylathais@gmail.com>
2024-06-16 23:29:25 -0300

    Toggle de Tema

--- assets/script.js
+++ assets/script.js
@@ -41,3 +41,12 @@ window.addEventListener('resize', () => {
         searchForm.classList.remove('show');
     }
 });
+const toggler = document.getElementById('theme-toggle');
+
+toggler.addEventListener('change', function () {
+    if (this.checked) {
+        document.body.classList.add('dark');
+    } else {
+        document.body.classList.remove('dark');
+    }
+});
